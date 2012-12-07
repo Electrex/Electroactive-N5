2c1d076844a05c8f6266cf418f61943b217dfec8
Alex Shi <alex.shi@intel.com>
2012-12-07 21:37:58 +0800

    sched: log the cpu utilization at rq
    The cpu's utilization is to measure how busy is the cpu.
            util = cpu_rq(cpu)->avg.runnable_avg_sum * SCHED_POEWR_SCALE
                    / cpu_rq(cpu)->avg.runnable_avg_period;
    
    Since the util is no more than 1, we scale its value with 1024, same as
    SCHED_POWER_SCALE and set the FULL_UTIL as 1024.
    
    In later power aware scheduling, we are sensitive for how busy of the
    cpu. Since as to power consuming, it is tight related with cpu busy
    time.
    
    BTW, rq->util can be used for any purposes if needed, not only power
    scheduling.
    
    Signed-off-by: Alex Shi <alex.shi@intel.com>
    
    Conflicts:
    	kernel/sched/sched.h

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -351,6 +351,9 @@ extern struct root_domain def_root_domain;
 
 #endif /* CONFIG_SMP */
 
+/* full cpu utilization */
+#define FULL_UTIL	SCHED_POWER_SCALE
+
 /*
  * This is the main, per-CPU runqueue data structure.
  *
@@ -477,7 +480,8 @@ struct rq {
 	struct llist_head wake_list;
 #endif
 
-        struct sched_avg avg;
+	struct sched_avg avg;
+	unsigned int util;
 };
 
 static inline int cpu_of(struct rq *rq)
