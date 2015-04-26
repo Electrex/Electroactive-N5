2c25a517dc15bd8b1d51571421cd5d7b98e8ba83
Yusuf Mostafa <Electrex@users.noreply.github.com>
2015-04-25 23:06:52 -0700

    Electroactive Governor

--- arch/arm/mach-msm/include/mach/kgsl.h
+++ arch/arm/mach-msm/include/mach/kgsl.h
@@ -45,6 +45,10 @@
 extern int graphics_boost;
 #endif
 
+#ifdef CONFIG_CPU_FREQ_GOV_ELECTROACTIVE
+extern int graphics_boost;
+#endif
+
 enum kgsl_iommu_context_id {
 	KGSL_IOMMU_CONTEXT_USER = 0,
 	KGSL_IOMMU_CONTEXT_PRIV = 1,
