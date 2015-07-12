8088f3a696d0cebeb797e4a5e1c888e855fac6ca
Yusuf Mostafa <ymostafa30@gmail.com>
2015-07-11 19:28:57 -0700

    Revert "PM: Introduce suspend state PM_SUSPEND_FREEZE"
    This reverts commit e4f1e00.

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -387,11 +387,6 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 		pr_info("wakeup source sensor_ind activate skipped\n");
 		return;
 	}
-        /*
-	 * active wakeup source should bring the system
-	 * out of PM_SUSPEND_FREEZE state
-	 */
-	freeze_wake();
 
 	ws->active = true;
 	ws->active_count++;
